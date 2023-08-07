6bce0d37c38f2a9ade1b525f6feb4ac1c6694a19
Yuya Shiratori <yuya.shiratori@access-company.com>
2023-08-07 16:59:14 +0900

    [239877] Bump up version to 0.2.1

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule AntikytheraAws.MixProject do
   def project() do
     [
       app: :antikythera_aws,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
